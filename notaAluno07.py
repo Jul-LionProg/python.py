ed0008e84669010415067239d0e369ed404765ca
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-02 11:12:43 -0300

    Update notaAluno07.py

--- notaAluno07.py
+++ notaAluno07.py
@@ -1,1 +1,4 @@
-
+not1 = float(input('Primeira Nota: '))
+not2 = float(input('Segundo Nota: '))
+media = not1 + not2
+print('Sua primeira nota foi {}, e a segunda nota foi {}, juntando as duas sua media final foi de {}:'.format(not1, not2, media))
