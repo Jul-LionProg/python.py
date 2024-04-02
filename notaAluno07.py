21dfa857bc6901728d76539d2f2df89e13189606
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-02 11:13:06 -0300

    Update notaAluno07.py

--- notaAluno07.py
+++ notaAluno07.py
@@ -2,3 +2,10 @@ not1 = float(input('Primeira Nota: '))
 not2 = float(input('Segundo Nota: '))
 media = not1 + not2
 print('Sua primeira nota foi {}, e a segunda nota foi {}, juntando as duas sua media final foi de {}:'.format(not1, not2, media))
+
+
+
+
+# DESAFIO 007 (Aula 07)
+# Desanvolva um programa que leia as duas notas de um aluno.
+# calcule e mostre a sua média.
