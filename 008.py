970c3ef012dfdee30308b94df1f56ae36b6f30b0
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-02 11:50:37 -0300

    Update 008.py

--- 008.py
+++ 008.py
@@ -1,1 +1,17 @@
+medida = float(input('Uma distancia em metros:'))
+cm = medida * 100
+mm = medida * 1000
+print('A medida em {}m corresponde a {:.0f}cm e {:.0f}mm'.format(medida, cm ,mm))
 
+
+
+# DESAFIO 008 (Aula 07)
+# Escreva um programa que leia um valor em metros e o exiba convertido em centrimetros e milimetros.
+
+#km
+#hm
+#dam
+#m
+#dm
+#cm
+#mm
