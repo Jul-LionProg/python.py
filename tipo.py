f9937965cd3bd964bf7a5b986085310e0f8e463a
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-02 10:03:42 -0300

    Update tipo.py

--- tipo.py
+++ tipo.py
@@ -7,3 +7,10 @@ print('É alfanumerico?', a.isalnum())
 print('Está em maiusculas?', a.isupper())
 print('Está em minisculas?', a.islower())
 print('Está capitalizada?', a.istitle())
+
+
+
+
+
+# DESAFIO 004 (Aula 06)
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu TIPOS PRIMITIVOS e todas as informações possiveis sobre ele.
