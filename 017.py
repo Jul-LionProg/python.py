410b8fa62587fff665fff12dec2a6820ad78ba06
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:50:49 -0300

    Update 017.py

--- 017.py
+++ 017.py
@@ -10,3 +10,9 @@ co = float(input('Comprimento do cateto oposto: '))
 ca = float (input('Comprimento do cateto adiacente: '))
 hi = math.hypot (co, ca) 
 print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+from math import hypot
+co = float(input('Comprimento do cateto oposto: ')) 
+ca = float (input('Comprimento do cateto adiacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
