8af410bfebb2925bce381b781738992972330431
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:05:46 -0300

    Update 010.py

--- 010.py
+++ 010.py
@@ -2,3 +2,6 @@ real  = float(input('Quanto dinheiro voce tem na carteira? R$'))
 dolar = real / 5.13
 euro  = real / 5.42
 pesos = real / 0.015
+print('Com R${:.2f} voce pode comprar US${:.2f}'.format(real, dolar))
+print('Com RS{:.2f} voce pode comprar EUR${:.2f}'.format(real, euro))
+print('Com R${:.2f} voce pode comprar ${:.2f}'.format(real, pesos))
