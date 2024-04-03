78102eb55aef1bfd2f092869d32b5349cd93ca2a
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:32:14 -0300

    Update 014.py

--- 014.py
+++ 014.py
@@ -2,3 +2,8 @@ c = float(input('Informe a temperatura em °C:'))
 f = ((9 * c) / 5) + 35 # ou f = 9 * c / 5 + 35
 print('A temperatura de {}°C corresponde a {}°F!'.format(c, f))
 
+
+
+
+# DESAFIO 014  (Aula 007)
+# Escreva um programa que converta uma temperatura digitada em °C e convertida em °F.
