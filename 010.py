fdc20dfd90e2dc0b66495c095c2aebf9fc9962d7
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:06:08 -0300

    Update 010.py

--- 010.py
+++ 010.py
@@ -5,3 +5,9 @@ pesos = real / 0.015
 print('Com R${:.2f} voce pode comprar US${:.2f}'.format(real, dolar))
 print('Com RS{:.2f} voce pode comprar EUR${:.2f}'.format(real, euro))
 print('Com R${:.2f} voce pode comprar ${:.2f}'.format(real, pesos))
+
+
+
+
+# DESAFIO 009 (Aula 07)
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares, euros e pesos ela pode comprar
