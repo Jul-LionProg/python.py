091015378e8e3ecd1f116fcea457263888a1d918
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:11:17 -0300

    Update 011.py

--- 011.py
+++ 011.py
@@ -4,3 +4,10 @@ alt = float(input('Altura da parede:'))
 print('Sau parede tem a dimensão de {} x {} e sua área é de {}m².'.format(larg, alt, área))
 tinta = área / 2
 print('Para pintar essa parede, voce precisara de {}L de tinta'.format(tinta))
+
+
+
+
+
+# DESAFIO 011 - (Aula 07)
+# Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua aŕea e a quantidade de tinta necessaria para pinta=la, sabendo que a  cada litro de tinta, pinta uma área de 2m².
