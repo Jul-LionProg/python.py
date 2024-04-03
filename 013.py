d643fb313f6b80167fc87e769a11cc7f4c5b79c9
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:28:17 -0300

    Update 013.py

--- 013.py
+++ 013.py
@@ -1,3 +1,9 @@
 salário = float(input('Qual é o salário do Funcionario? R$:'))
 novo = salário + (salário * 30 / 100)
 print('Um funcionario que ganhava R${:.2f}, com 30% de aumento, passa a receber R${:.2f}'.format(salário, novo))
+
+
+
+
+# DESAFIO 013 (Aula 007)
+# Faça um algoritmo que leia o salário de um funcionario e mostre seu novo salário, com 30% de aumento
