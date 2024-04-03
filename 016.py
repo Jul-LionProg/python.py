a4442a8ef1008519247403109f979620d78f90fa
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:44:40 -0300

    Create 016.py

---
+++ 016.py
@@ -0,0 +1,4 @@
+from math import trunc '''OU  import math '''
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} ea sua porção inteira é {} '.format(num, trunc(num))), '''OU .format(num, math.trunc(num)),'''
+
