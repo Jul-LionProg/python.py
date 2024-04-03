a8b8769605f112dc0437ff774afb02a0000a7514
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:52:16 -0300

    Update 017.py

--- 017.py
+++ 017.py
@@ -16,3 +16,8 @@ co = float(input('Comprimento do cateto oposto: '))
 ca = float (input('Comprimento do cateto adiacente: '))
 hi = hypot(co, ca)
 print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+
+
+# DEFASIO 017  (Aula08)
+# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcula e mostre o comprimento da hipotenusa.
