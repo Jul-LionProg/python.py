5aa993b676a947b143922d2cd0dd2aa09e1ef653
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:10:38 -0300

    Update 011.py

--- 011.py
+++ 011.py
@@ -1,3 +1,6 @@
 larg = float(input('Largura da parede:'))
 alt = float(input('Altura da parede:'))
 área = larg*alt
+print('Sau parede tem a dimensão de {} x {} e sua área é de {}m².'.format(larg, alt, área))
+tinta = área / 2
+print('Para pintar essa parede, voce precisara de {}L de tinta'.format(tinta))
