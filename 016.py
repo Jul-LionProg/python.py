2c57d13280ae2e2c8af1148c3843518b259c0acc
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-03 17:45:06 -0300

    Update 016.py

--- 016.py
+++ 016.py
@@ -2,3 +2,7 @@ from math import trunc '''OU  import math '''
 num = float(input('Digite um valor: '))
 print('O valor digitado foi {} ea sua porção inteira é {} '.format(num, trunc(num))), '''OU .format(num, math.trunc(num)),'''
 
+
+'''   OU
+num = float(input('Digite um valor:' ))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num,int(num))),'''
