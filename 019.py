746fd7e20e25df6f8192dd06650680beb5f39273
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-04 11:50:51 -0300

    Update 019.py

--- 019.py
+++ 019.py
@@ -3,3 +3,6 @@ n1 = str(input('Primeiro aluno:'))
 n2 = str(input('segundo aluno:'))
 n3 = str(input('Treceiro aluno:'))
 n4 = str(input('quarto aluno:'))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido foi VC{}'.format(escolhido))
