e5072693740ef09b1bf19812b4a5714d78d12c68
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-04 11:51:40 -0300

    Update 019.py

--- 019.py
+++ 019.py
@@ -6,3 +6,8 @@ n4 = str(input('quarto aluno:'))
 lista = [n1, n2, n3, n4]
 escolhido = choice(lista)
 print('O aluno escolhido foi VC{}'.format(escolhido))
+
+
+# DESAFIO 019 (Alua 08)
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+# Faça um programa que aajude ele, lendo o nome deles e escrevendo o nome do escolhido.
