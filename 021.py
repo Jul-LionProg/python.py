aa9914a8f6a32171e8e48792576a480f8f74de5c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-04 12:13:53 -0300

    Update 021.py

--- 021.py
+++ 021.py
@@ -2,3 +2,7 @@ import pygame
 pygame.init()
 pygame.mixer.music.load('meu do arquivo  ex021.mp3')
 pygame.mixer.music.play()
+pygame.event.wait()
+
+# DEFASIO 021 (Aula 08)
+# Faça um programa em Python que abra uma arquivo com audio em MP3. Estudar o 'pygame'  mais utilizado para jogos mais da pra fazer tipo um despertador.
