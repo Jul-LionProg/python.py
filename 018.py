66e3021b6b803b1986d6757e8101df7f56313dde
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-04 11:47:09 -0300

    Update 018.py

--- 018.py
+++ 018.py
@@ -15,3 +15,10 @@ print('O angulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
 cosseno = cos(radians(angulo))
 print('O angulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
 tangente = tan(radians(angulo))
+
+
+
+# DESEFIO 018 (aula 08)
+# Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno, cosseno, tangente desse angulo.
+
+# print('O angulo de {} tem a TANGENTE de {:.2f}'.format(angulo, tangente))
