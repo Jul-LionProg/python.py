31128f4e4dce76ce02b323cf2f0fa10a66449146
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-04 12:06:55 -0300

    Update 020.py

--- 020.py
+++ 020.py
@@ -7,3 +7,9 @@ lista = [n1, n2, n3, n4]
 shuffle(lista)
 print('A ordem de apresentação sera!')
 print(lista)
+
+
+
+# DESAFIO 020 (Aula 08)
+# O mesmo profesor do desafior anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
+# Faça um programa que leia o nome dos alunose mostre a ordem sorteada.
