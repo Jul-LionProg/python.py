e98d534d3417e2788e60b5121ee97594fc6a50c5
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:11:16 -0300

    Update 023.py

--- 023.py
+++ 023.py
@@ -5,3 +5,6 @@ print('Milhar: {}'.format(n[0]))
 print('Centena: {}'.format(n[1]))
 print('Dezena: {}'.format(n[2]))
 print('Unidade: {}'.format(n[3]))'''
+
+num =  int(input('Informe um numero: '))
+u = num // 1 % 10
