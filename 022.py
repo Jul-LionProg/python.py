51a6e27136913d2c72a2416d43dea7e196294f24
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:04:47 -0300

    Update 022.py

--- 022.py
+++ 022.py
@@ -1,2 +1,4 @@
 nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome...')
+print('Seu nome em mausculo é {}'.format(nome.upper()))
+print('Seu nomeem minusculo é {}'.format(nome.lower()))
