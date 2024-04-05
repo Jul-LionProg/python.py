1918e504d78849bfba2671362ee88ef8f5a41038
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:10:59 -0300

    Update 023.py

--- 023.py
+++ 023.py
@@ -4,3 +4,4 @@ print('Analisando o numero {}'.format(num))
 print('Milhar: {}'.format(n[0]))
 print('Centena: {}'.format(n[1]))
 print('Dezena: {}'.format(n[2]))
+print('Unidade: {}'.format(n[3]))'''
