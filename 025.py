628602de4a2ad0e41bbb2feef0582f7a8473a068
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:27:47 -0300

    Update 025.py

--- 025.py
+++ 025.py
@@ -1,3 +1,7 @@
 nome = str(input('Qual é seu nome Comlete?')).strip()
 print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
 
+
+
+# DESAFIO 025 (Aula 09)
+# Crie um programa que leia o nome de uma pessoa e diga se tem "SILVA" no nome.
