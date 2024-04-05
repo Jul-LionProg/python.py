a3de1f22694bde1f1435707d46417a34e9c126f9
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:05:56 -0300

    Update 022.py

--- 022.py
+++ 022.py
@@ -5,3 +5,10 @@ print('Seu nomeem minusculo é {}'.format(nome.lower()))
 print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
 #print('Seu nome tem ao todo {} letras'.format(nome.find(' ')))
 separa = nome.split()
+
+
+# DESAFIO 022  (Aula 09)
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# *O nome com todas as letras mausculas e minusculas.
+# *Quantas letras ao todo (sem considerar espaços).
+# *Quantas letras tem o primeiro nome.
