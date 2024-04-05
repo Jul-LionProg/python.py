8be42586acf0688fb30b6453716cd6eaf7d47d1e
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:23:14 -0300

    Update 024.py

--- 024.py
+++ 024.py
@@ -1,2 +1,7 @@
 cid  = str(input('Em qual cidade voce nasceu?')).strip()
 print(cid[:5].upper() == 'SANTO')
+
+
+
+# DESAFIO 024 (Aula 09)
+# Crei um programa que leia o nome da cidadee digo se o nome começa com "SANTO".
