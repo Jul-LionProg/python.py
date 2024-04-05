14dc0ab2adf297d905f962394e27c7442731a7bc
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:44:01 -0300

    Update 026.py

--- 026.py
+++ 026.py
@@ -8,3 +8,4 @@ print('A última letra A apareceu na posição {}'.format(frase.rfind('A')+1))
 # Faça um programa que leia uma frase pelo teclado e mostre:
 # * Quantos vezes aparece a leia "A".
 # * Em que posição ela aparece a primeira vez.
+# * Em que posição ela aparece a última vez. 
