332e4aaeb46052b1127d9b0abc5f430d760cace7
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-05 10:43:27 -0300

    Update 026.py

--- 026.py
+++ 026.py
@@ -2,3 +2,7 @@ frase = str(input('Digite uma frase:')).upper().strip()
 print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
 print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
 print('A última letra A apareceu na posição {}'.format(frase.rfind('A')+1))
+
+
+# DESAFIO 026 (Aula 09)
+# Faça um programa que leia uma frase pelo teclado e mostre:
