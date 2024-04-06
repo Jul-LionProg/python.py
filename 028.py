fabeabb9c9ac416b17bf60bf39b5634434cfb90c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:24:30 -0300

    Update 028.py

--- 028.py
+++ 028.py
@@ -4,3 +4,5 @@ computador = randint(0, 6) # Faz o computador "PENSAR"
 print('-=-' * 20)
 print('Vou pensar em um número entra 0 e 6. Tente adivinhar...')
 print('-=-' *20)
+jogador = int(input('Em que número eu pensei?')) # Jogador tentar adivinhar o numero
+print('PROCESSANDO....')
