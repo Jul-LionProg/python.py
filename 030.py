1bcad8025b0d3149676e84a22d34778af060cc7d
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 15:31:39 -0300

    Update 030.py

--- 030.py
+++ 030.py
@@ -3,3 +3,5 @@ resultado = numero % 2  # todo resto de um numero (impar(1) ou par(0)) dividido 
 if resultado == 0:
     print('O Número {} é PAR!'.format(numero))
 else:
+    print('O número {} é IMPAR'.format(numero))
+    
