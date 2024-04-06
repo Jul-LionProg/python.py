ab6169dfdc35ef964a6a984fcb462479ebcd3c5e
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 15:42:24 -0300

    Update 031.py

--- 031.py
+++ 031.py
@@ -5,3 +5,4 @@ preço = distancia * 0.50 if distancia <= 200 else distancia *0.45 # + Pratico e
      preço = distancia * 0.50
    else:
      preço = distancia * 0.45'''
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))
