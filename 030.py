7a9b844abafbde7157b10405f8f59f78c467d020
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 15:31:27 -0300

    Update 030.py

--- 030.py
+++ 030.py
@@ -1,3 +1,5 @@
 numero = int(input('Me diga um número: '))
 resultado = numero % 2  # todo resto de um numero (impar(1) ou par(0)) dividido por %2 vai dar (0 ou 1) 
 if resultado == 0:
+    print('O Número {} é PAR!'.format(numero))
+else:
