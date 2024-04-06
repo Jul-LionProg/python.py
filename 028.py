ad9322ddf1b7e5668f5e9ea7f8768f19fe412d46
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:24:41 -0300

    Update 028.py

--- 028.py
+++ 028.py
@@ -6,3 +6,5 @@ print('Vou pensar em um número entra 0 e 6. Tente adivinhar...')
 print('-=-' *20)
 jogador = int(input('Em que número eu pensei?')) # Jogador tentar adivinhar o numero
 print('PROCESSANDO....')
+sleep(4) # segundos de espera....
+if jogador == computador:
