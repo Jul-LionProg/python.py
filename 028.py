5928a1df39fbc199fd30118457000abdf9415d4a
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:26:02 -0300

    Update 028.py

--- 028.py
+++ 028.py
@@ -13,3 +13,9 @@ else:
     print('GANHEI!! Eu pensei no número {} e não no {}!'.format(computador, jogador))
 
 
+
+
+# DESAFIO 028 (aULA 010)]
+# Escreva um programa que faça o computador "pensar em um n´umero inteiro entra" 0 e 6
+# E peça o usuario tentar descobrir qual foi o número escolhido pelo computador.
+# O programa devera escrever na tela se o usuaio VENCEU ou PERDEU.
