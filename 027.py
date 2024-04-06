84cdb51a58777542198db67aff4d4cb0be396736
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:21:07 -0300

    Update 027.py

--- 027.py
+++ 027.py
@@ -3,3 +3,11 @@ nome = n.split()
 print('Muito prazer em te conhecer!')
 print('Seu primeiro nome é {}'.format(nome[0]))
 print('Seu último nome é {}'.format(nome[len(nome)-1]))
+
+
+
+# DESAFIO 027 (Aula 09)
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+# EX: Ana Maria de Souza
+ # primeiro = Ana
+ # Ùlitimo  = Souza
