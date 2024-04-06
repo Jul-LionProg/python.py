5c45fc072119dc54b2ddb2fcff153f6e638308ab
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:24:20 -0300

    Update 028.py

--- 028.py
+++ 028.py
@@ -2,3 +2,5 @@ from random import randint
 from time import sleep
 computador = randint(0, 6) # Faz o computador "PENSAR"
 print('-=-' * 20)
+print('Vou pensar em um número entra 0 e 6. Tente adivinhar...')
+print('-=-' *20)
