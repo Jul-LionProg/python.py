4173cd031fcde9e67ea4c387ef3c28772bbf18f1
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 15:42:44 -0300

    Update 031.py

--- 031.py
+++ 031.py
@@ -6,3 +6,8 @@ preço = distancia * 0.50 if distancia <= 200 else distancia *0.45 # + Pratico e
    else:
      preço = distancia * 0.45'''
 print('E o preço da sua passagem será de R${:.2f}'.format(preço))
+
+
+# DESAFIO 031 (aula 010)
+# Desenvolva um programa que pergunte a distancia de uma viagem em Km.
+# Calcule o preço da passagem,cobrando R$0,50 por Km para viagens até 200Km e R$0,45 para viagens mais longas. 
