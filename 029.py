d38e9591357e7439b1ec19271490b4704bf4c96f
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:30:30 -0300

    Update 029.py

--- 029.py
+++ 029.py
@@ -1,5 +1,10 @@
 velocidade = float(input('Qual é a velocidade atual do carro? '))
 if velocidade > 80:
+    print('MULTADO! Vocẽ excedeu o limite permitido é de 80Km/h')
+    multa = (velocidade-80) * 7
+
+
+
 
 # DESAFIUO 029 (Aula 010)
 # Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem, Dizendo 
