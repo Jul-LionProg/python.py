f5506b774b07d172681ca7ab126dc2a1ab1854b3
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 15:41:51 -0300

    Update 031.py

--- 031.py
+++ 031.py
@@ -1,3 +1,4 @@
 distancia = float(input('Qual é a distancia da sua viagem? '))
 print('Vocẽ esta preste a começar uma viagem de {}Km.'.format(distancia))
-
+preço = distancia * 0.50 if distancia <= 200 else distancia *0.45 # + Pratico e resumindo
+'''if distancia <= 200:
