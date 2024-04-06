a92be6aa24b3729b3f76cd50ef691fb078f9179d
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-06 07:30:15 -0300

    Update 029.py

--- 029.py
+++ 029.py
@@ -1,1 +1,7 @@
+velocidade = float(input('Qual é a velocidade atual do carro? '))
+if velocidade > 80:
 
+# DESAFIUO 029 (Aula 010)
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem, Dizendo 
+# "MULTADO".
+# A multa vai custar R$7,00 por cada KM acima do limite.
