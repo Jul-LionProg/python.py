8ad5fdac693e0ef29d982157de2b493fcc6aef14
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:18:39 -0300

    Update 034.py

--- 034.py
+++ 034.py
@@ -4,3 +4,11 @@ if salario <= 1250:
 else:
     novo = salario + (salario * 10 / 100)
 print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora'.format(salario, novo))
+
+
+
+
+# DESAFIO 034 (aula 010)
+# Escreva um programa que pergunte o salário de um funcionario e calcule o valor do seu aumento.
+# Para salários superiores a 1.250,00 calcule um aumento de 10%.
+# Para salários inferior ou iguais, o aumento é de 15%.
