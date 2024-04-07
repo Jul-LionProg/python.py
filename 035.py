404eac012ba7fee8411f789524a64b429500ef7d
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:26:58 -0300

    Update 035.py

--- 035.py
+++ 035.py
@@ -8,3 +8,7 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
      print('Os Segmentos acima Podem Forma Um Triangulo! ')
 else:
     print('Os Segmentos acima NÂO Podem Forma um Triangulo!')
+
+
+# DESAFIO 035 (Aula 010)
+# Desenvolva um programa que leia o comprimento de tres retas e digga ao usuario se elas podem ou não formar um triangulo.
