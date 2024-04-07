4931f011f1cea81d0ade8a030d81d97c496ce886
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:05:00 -0300

    Update 032.py

--- 032.py
+++ 032.py
@@ -4,3 +4,6 @@ if ano == 0:
     ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} NAO é BISSEXTO'.format(ano))
+ 
