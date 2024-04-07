b6557de7ed50630ed046c848e159a5b0a4cf5a95
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:18:12 -0300

    Update 034.py

--- 034.py
+++ 034.py
@@ -2,3 +2,5 @@ salario = float(input('Qual é o salaŕio do funcionario? R$'))
 if salario <= 1250:
     novo = salario + (salario * 15 / 100) # Para usar desconto usa -
 else:
+    novo = salario + (salario * 10 / 100)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora'.format(salario, novo))
