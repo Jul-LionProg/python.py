4b2197e24487922569f29e08e487b9a53aace661
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:04:25 -0300

    Update 032.py

--- 032.py
+++ 032.py
@@ -1,3 +1,6 @@
 from datetime import date # Pesquisar a data e hora do seu pc atual (date) só pesquisa a data
 ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
 if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
