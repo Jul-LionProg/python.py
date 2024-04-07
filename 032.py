a93fc0be997c1a212c1bc23572168fa22b33c310
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:04:08 -0300

    Update 032.py

--- 032.py
+++ 032.py
@@ -1,1 +1,3 @@
-
+from datetime import date # Pesquisar a data e hora do seu pc atual (date) só pesquisa a data
+ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+if ano == 0:
