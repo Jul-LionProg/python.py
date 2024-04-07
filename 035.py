94c37e5e6e5d1ec8e615313f757167e3d555c804
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-07 13:25:51 -0300

    Update 035.py

--- 035.py
+++ 035.py
@@ -5,4 +5,5 @@ r1 = float(input('Segmento 1'))
 r2 = float(input('Segmento 2'))
 r3 = float(input('Segmento 3'))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
- 
+     print('Os Segmentos acima Podem Forma Um Triangulo! ')
+else:
