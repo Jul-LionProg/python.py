5534d06af125870e4af5d49a4b7b6c7ea883ea64
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:01:36 -0300

    Update 037.py

--- 037.py
+++ 037.py
@@ -1,2 +1,5 @@
 num = int(input('Digite um numero inteiro:'))
 print(''' Escolha uma das bases para conversão:
+[ 1 ] Converter para BINARIO
+[ 2 ] Converter para OCTAL
+[ 3 ] Converter para HEXADECIMAL ''')
