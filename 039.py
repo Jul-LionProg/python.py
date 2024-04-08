147e31559af2a12935b821d67e15e299b030f5ed
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:17:49 -0300

    Update 039.py

--- 039.py
+++ 039.py
@@ -4,3 +4,5 @@ nasc = int(input('Ano de nascimento: '))
 idade = atual - nasc
 print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
 if idade == 18:
+     print('Voce tem que se alistar IMEDIATAMENTE')
+elif idade < 18:
