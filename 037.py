59b50cc4d0a27889b85fc7a333713a8350444c82
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:01:49 -0300

    Update 037.py

--- 037.py
+++ 037.py
@@ -3,3 +3,5 @@ print(''' Escolha uma das bases para conversão:
 [ 1 ] Converter para BINARIO
 [ 2 ] Converter para OCTAL
 [ 3 ] Converter para HEXADECIMAL ''')
+opçao = int(input('Sua opção: '))
+if opçao == 1:
