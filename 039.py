7fa2accebded045974861024a6a7a1775170553c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:18:03 -0300

    Update 039.py

--- 039.py
+++ 039.py
@@ -6,3 +6,5 @@ print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
 if idade == 18:
      print('Voce tem que se alistar IMEDIATAMENTE')
 elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda falta {} anos para o alistamento'.format(saldo))
