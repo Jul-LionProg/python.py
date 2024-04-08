e5bbf6066df544913a015e08c57a40d6fb7037ea
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:17:41 -0300

    Update 039.py

--- 039.py
+++ 039.py
@@ -2,3 +2,5 @@ from datetime import date
 atual = date.today().year
 nasc = int(input('Ano de nascimento: '))
 idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
