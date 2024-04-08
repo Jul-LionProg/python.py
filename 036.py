554f4f2b9e4d75260ea4027bbd82f3cf5997714b
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 11:55:30 -0300

    Update 036.py

--- 036.py
+++ 036.py
@@ -2,3 +2,5 @@ casa  = float(input('Valor da casa: R$'))
 salario = float(input('salario de comprador: R$'))
 anos = int(input('Quantos anos de financiamento?'))
 prestaçao = casa / (anos * 12)
+minino = salario * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos),end='')
