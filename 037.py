1a6bdcdeef4ff8eddca4dd8da921b5b49c0f8333
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:02:20 -0300

    Update 037.py

--- 037.py
+++ 037.py
@@ -9,3 +9,5 @@ if opçao == 1:
 elif opçao == 2:
     print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
 elif opçao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
