61f1130661844e83ac5c274017bba618d889c0cd
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 11:55:40 -0300

    Update 036.py

--- 036.py
+++ 036.py
@@ -4,3 +4,6 @@ anos = int(input('Quantos anos de financiamento?'))
 prestaçao = casa / (anos * 12)
 minino = salario * 30 / 100
 print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos),end='')
+if prestaçao <= minino:
+    print('Empréstimo pode ser \033[32mCONCEDIDO\033[m!')
+else:
