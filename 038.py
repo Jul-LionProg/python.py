c92090cc46acb4250c3479184ee1943cee722d62
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-08 12:12:33 -0300

    Update 038.py

--- 038.py
+++ 038.py
@@ -6,3 +6,10 @@ elif n2 > n1:
     print('O Segundo valor é MAIOR')
 else:
     print('Os dois valores são IGUAIS')
+ 
+
+# DASEFIO 038 (Aula 012)
+# Escreva um programa que leia dois numeros inteiros e compare-os, mostrando na tela uma mensagem:
+'''- O Primeiro valor ẽ MAIOR
+   - O Segundo valor ẽ MAIOR
+   - Não existe valor maior os dois são iguais'''
