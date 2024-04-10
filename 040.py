d861f4c814313e4367eb39a3d669c765a9c9b1a6
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-10 10:26:34 -0300

    Update 040.py

--- 040.py
+++ 040.py
@@ -3,3 +3,5 @@ nota2 = float(input('Segunda nota: '))
 media = (nota1 + nota2) / 2
 print('Tirando {:.1f} e {:.1f} a media do aluno é {:.1f}'.format(nota1, nota2, media))
 if  7 > media >= 5:
+    print('O aluno esta em RECUPERAÇÃO. ')
+elif media >= 5:
