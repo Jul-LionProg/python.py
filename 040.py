00f977fc9a615d6a9b6cd92cf37d1a224055b9ef
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-10 10:25:42 -0300

    Update 040.py

--- 040.py
+++ 040.py
@@ -1,3 +1,5 @@
 nota1 = float(input('Primeiro nota: '))
 nota2 = float(input('Segunda nota: '))
-
+media = (nota1 + nota2) / 2
+print('Tirando {:.1f} e {:.1f} a media do aluno é {:.1f}'.format(nota1, nota2, media))
+if  7 > media >= 5:
