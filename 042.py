17351ca5cd2756222fd294e364b544ea306d91bc
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-12 14:05:24 -0300

    Update 042.py

--- 042.py
+++ 042.py
@@ -4,3 +4,5 @@ r3 = float(input('Terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM FORMAR um triangulo ', end='')
     if r1 == r2 == r3:
+       print('EQUILATERO!')
+    elif r1 != r2 != r3 != r1:
