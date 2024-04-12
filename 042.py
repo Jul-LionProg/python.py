b83e87f8afd9dcc74e4be2bfc73e1b6fa0bdc98c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-12 14:05:33 -0300

    Update 042.py

--- 042.py
+++ 042.py
@@ -6,3 +6,6 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     if r1 == r2 == r3:
        print('EQUILATERO!')
     elif r1 != r2 != r3 != r1:
+    print('ESCALENDO!')
+    else:
+        print('ISOSCELES!')
