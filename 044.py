f743ac454d2cc9a351f68f763d7c023ce650b3f7
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-12 14:24:28 -0300

    Update 044.py

--- 044.py
+++ 044.py
@@ -2,3 +2,5 @@ preço = float(input('Preço das compras: R$ '))
 print('''FORMAS DE PAGAMENTO
 [ 1 ] Á vista dinheiro/Pix
 [ 2 ] Á vita no cartão
+[ 3 ] 2x no cartão S/juros
+[ 4 ] 3x ou mais C/juros''')
