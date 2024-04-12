bd63a442a3f77e81bac8d1aa740025b173076c70
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-12 14:10:40 -0300

    Update 043.py

--- 043.py
+++ 043.py
@@ -1,1 +1,7 @@
 
+
+
+# DESAFIO 043 (Aula 012)
+# Desenvolva um a logica que leia o peso ea altura se uma pessoa calculando o sei IMC e mostre seu status de acordo com a tabela abaixo.
+'''- Abaixo de 18.5: Abaixo de peso     - 25 ate 30: Sobrepeso
+   - Entre 18.5 e 25: Peso ideal        - 30 ate 40: Obesidad    - Acima de 40: Obesidade Morbida'''
