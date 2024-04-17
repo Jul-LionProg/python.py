6dd2854b100e80c27835a4b3b8cb77c806d23844
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:49:37 -0300

    Update 049.py

--- 049.py
+++ 049.py
@@ -1,3 +1,8 @@
 num = int(input('Digite um numero para ver sua tabuada: '))
 for c in range(1, 11):
     print('{} x {:2} = {}'.format(num, c, num*c))
+
+
+# DESAFIO (Aula 013)
+# Refaça o (DESAIO 009) Só que agora utilisando 
+# LOÇO [FOR].
