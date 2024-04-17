17b946b60c016e5fabd01f662c03c80aa5534475
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:45:13 -0300

    Update 048.py

--- 048.py
+++ 048.py
@@ -5,3 +5,8 @@ for c in range(1, 501, 2):
         cont = cont + 1
         soma = soma + c    # soma += c 
 print('A soma de todos os {} valores soliacitados é {}'.format(cont, soma))
+
+
+
+# DESAFIO 048 (Aula 013)
+# Faça um programa que calcule a soma entre todos os numeros impares que são multiplos de 3 e que se encontram no intervalo de 1 a 500.
