2b55ec52461b893d58b6c95e5e701c9101518f41
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:59:08 -0300

    Update 051.py

--- 051.py
+++ 051.py
@@ -1,3 +1,5 @@
 primeiro = int(input('Primeiro Termo '))
 razao = int(input('Razão: '))
 decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print('{}'.format(c), end='➝ ')
