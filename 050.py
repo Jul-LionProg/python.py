f6f502702f08c4be4c0f1d1d935b554b62ab7c08
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:54:24 -0300

    Update 050.py

--- 050.py
+++ 050.py
@@ -3,3 +3,6 @@ cont = 0
 for c in range(1, 7):
     num = int(input('Digite o {} valor' .format(c)))
     if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Voce informou {} numero PARES e a soma foi {}'.format(cont, soma))
