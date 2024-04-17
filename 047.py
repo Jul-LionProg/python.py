651a8af7110543b5b51f62513a8bbfd6cbd75119
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:09:44 -0300

    Update 047.py

--- 047.py
+++ 047.py
@@ -2,4 +2,7 @@ for n in range(2, 51, 2):
     print(n, end=' ')
 print('Acabou!!')
 
-#defasio 047 (Aula 013) 
+
+
+# Desafio 047 (Aula 013) novo mundo 02
+# Crie um programa que mostre na tela todos os numeros PARES que estão no intervalo entre 1 e 50.
