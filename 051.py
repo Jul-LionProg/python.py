9ba7f477fa027cc3c02190a652f5f10808b951b6
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 17:00:18 -0300

    Update 051.py

--- 051.py
+++ 051.py
@@ -4,3 +4,10 @@ decimo = primeiro + (10 - 1) * razao
 for c in range(primeiro, decimo + razao, razao):
     print('{}'.format(c), end='➝ ')
 print('ACABOU')
+
+
+# DESAFIO 051 (Aula 013)
+# Desenvolva um programa que leia o PRIMEIRO TERMO e a RAZAO de uma (PA)
+# No final mostre os 10 primeiros termos dessa progressão.
+# Termo = contar 0 a ....
+# PA = pular de 2 em 2 ou (4 em 4) (7 em 7) etcs...
