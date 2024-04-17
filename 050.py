1a9d2df7c5372fc90547fac385c1c7962a1e43ef
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:54:45 -0300

    Update 050.py

--- 050.py
+++ 050.py
@@ -6,3 +6,7 @@ for c in range(1, 7):
         soma += num
         cont += 1
 print('Voce informou {} numero PARES e a soma foi {}'.format(cont, soma))
+
+
+# DESAFIO 050 (Aula 013)
+# Desenvolva um programa que leia 6 numeros int e mostre a soma apenas daqueles que forem PAR. Se o valor for IMPAR. Desconsidere.
