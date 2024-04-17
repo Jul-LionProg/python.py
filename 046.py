d4afb37eb49cf6e38c77b6c7e8af46f4a9574e10
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-17 16:03:07 -0300

    Update 046.py

--- 046.py
+++ 046.py
@@ -3,3 +3,7 @@ for cont in range(10, -1, -1):
     print(cont)
     sleep(0.9)
 print('Feliz ANO NOVOOO!!')
+
+
+# DESAFIO 46 (Aula 013) novo mundo 02
+# Faça um programa que mostre na tela uma contagem regressiva para ano novo de 10 a 0 com pausa de 0.9 segundos
