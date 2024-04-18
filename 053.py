d60e5003a8cd00f2938c7a7db820e33c69750076
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-18 10:31:22 -0300

    Update 053.py

--- 053.py
+++ 053.py
@@ -1,2 +1,4 @@
 frase = str(input('Frase: ')).strip().upper() #.strip(eliminar os espaço antes e depois)
 palavras = frase.split()   #.split(separa o vetor todo em uma lista)
+junto = ''.join(palavras)  #.join(juntar tudo em uma string só)
+inverso = ''
