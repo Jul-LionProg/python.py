ecd4a1e0716d126a4b39d6ba3359911bce2c17a5
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-18 10:31:33 -0300

    Update 053.py

--- 053.py
+++ 053.py
@@ -2,3 +2,5 @@ frase = str(input('Frase: ')).strip().upper() #.strip(eliminar os espaço antes 
 palavras = frase.split()   #.split(separa o vetor todo em uma lista)
 junto = ''.join(palavras)  #.join(juntar tudo em uma string só)
 inverso = ''
+for letra in range(len(junto)-1, -1, -1): # len foi da ultima letra ate a -1 primeria pulando um -1
+    inverso += junto[letra]
