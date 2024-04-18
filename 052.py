05d64d4d876b22238543f3973012606dfa24944b
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-18 10:27:51 -0300

    Update 052.py

--- 052.py
+++ 052.py
@@ -9,3 +9,5 @@ for c in range(1, num + 1):
     print('{}'.format(c), end='')
 print('\n\033[m0 número {} foi divisivel {} vezes'.format(num,tot))
 if tot == 2:
+    print('E por isso ele é PRIMO!')
+else:
