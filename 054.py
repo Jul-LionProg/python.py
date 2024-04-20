3476f73a33310a1aa537a54541c8bb45d1d98cc1
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-20 16:05:15 -0300

    Update 054.py

--- 054.py
+++ 054.py
@@ -9,3 +9,6 @@ for pess in range(1, 8):
         totmaior += 1
     else:
         totmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
+print('E tambem tivemos {} pessoas menores de idade'.format(totmenor))
+
