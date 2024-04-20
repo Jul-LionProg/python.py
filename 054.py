e7f0ced5bedf96d4f04a66cf2864678692243f93
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-20 16:04:50 -0300

    Update 054.py

--- 054.py
+++ 054.py
@@ -2,3 +2,6 @@ from datetime import date
 atual = date.today().year
 totmaior = 0
 totmenor = 0
+for pess in range(1, 8):
+    nasc = int(input('Em que ano a {} pessoa nasceu?'.format(pess)))
+    idade = atual - nasc
