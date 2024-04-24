85bc2590be65bf054df32a068ba05e4b2ddd1907
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-24 16:32:38 -0300

    Update 056.py

--- 056.py
+++ 056.py
@@ -7,3 +7,4 @@ for p in range(1, 5):
     print('-----{}ª PESSOA -----'.format(p))
     nome = str(input('Nome: ')).strip() # tirar os espaços
     idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
