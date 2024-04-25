8406d005f72a1a8916e30b3fec1a24e97b9ca651
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-04-25 15:38:38 -0300

    Update 057.py

--- 057.py
+++ 057.py
@@ -1,2 +1,3 @@
 sexo = str(input('Informe seu sexo [M/F]')).strip().upper()[0] # pegar a primeria Letra
 while sexo not in 'MmFf': # enquanto
+    sexo - str(input('Dados invalidos. Por favor, informe seu sexo: ')).strip().upper()[0]
