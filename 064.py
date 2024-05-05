19e734a08a03cdc46ebd0c20c1cd445e5b84502b
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-05 16:16:58 -0300

    Update 064.py

--- 064.py
+++ 064.py
@@ -10,3 +10,7 @@ print('Voce digitou  {} numeros e a soma entre eles foi {}.'.format(cont, soma))
 
 # DESAFIO 064  (Aula 14)
 # Crie um progroma qie leia varios numeros inteiros pelo teclado.
+'''O progroma so vai papar quando o usuario digitar o valor 999, que
+é a condição de parda.
+No final mostre quantos numeros foram digitados e qual foi a soma entre eles
+(desconsiderando o flag)'''
