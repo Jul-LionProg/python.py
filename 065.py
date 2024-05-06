cf08c6d9a1d034ee7bac4f1f897d4d4d5cdb3ab0
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-06 15:28:50 -0300

    Update 065.py

--- 065.py
+++ 065.py
@@ -14,3 +14,4 @@ while resp in 'Ss':  # entre
     resp = str(input('Quer continuar? [N/S] ')).upper().strip()[0]
 media = soma / quant
 print('Voce digitou {} numero e a media foi {}'.format(quant, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
