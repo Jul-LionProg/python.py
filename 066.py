63572d0cd76956f1b905bdd6b06ffc7860596117
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-08 16:24:58 -0300

    Update 066.py

--- 066.py
+++ 066.py
@@ -5,4 +5,7 @@ while True:
      break
   cont += 1
   soma += num
-print(f'A soma dos {cont} valores foi {}!') 
+print(f'A soma dos {cont} valores foi {}!') # f' ' striong
+
+# DESAFIO 066 (Aula 15)
+# Crie um programa que leia varios numeros inteiros. O programa so vap para quando o usuario digitar 999, que é Aula
