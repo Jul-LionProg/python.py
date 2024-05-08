7ed8d005078e3baba4af7eedc69581e2f94bf0ce
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-08 16:27:18 -0300

    Update 066.py

--- 066.py
+++ 066.py
@@ -1,11 +1,11 @@
 soma = cont = 0
 while True:
-  num = int(input('digite um valor(999 para parar): '))
-  if num == 999:
-     break
-  cont += 1
-  soma += num
-print(f'A soma dos {cont} valores foi {}!') # f' ' striong
+    num = int(input('Digite um valor (999 para parar): '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'A soma dos {cont} valores foi {soma}!') # f string
 
 # DESAFIO 066 (Aula 15)
 # Crie um programa que leia varios numeros inteiros. O programa so vap para quando o usuario digitar 999, que é Aula
