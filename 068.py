6bc496a580cd51d5d683998483addc8ebcda7a12
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-10 17:32:54 -0300

    Update 068.py

--- 068.py
+++ 068.py
@@ -28,4 +28,6 @@ print(f'GAME OVER! Você venceu {v} vezes.')
 
 
 
-EXERCÍCIO 068: Jogo do Par ou Ímpar
+# DESAFIO 068  (AULA)
+# Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o
+# jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
