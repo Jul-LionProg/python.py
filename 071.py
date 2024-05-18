8630b9e93d21cd5c6d74a4081e0bcf31efee618c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-18 15:57:30 -0300

    Update 071.py

--- 071.py
+++ 071.py
@@ -26,8 +26,11 @@ print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
 
 
 
-"""
-DESAFIO 071:()
-Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao
+
+# DESAFIO 071:()
+'''Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao
 usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas
 cédulas de cada valor serão entregues.
+
+OBS: Considere que o caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
+'''
