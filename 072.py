c897a39316a071d88dae8a410c803b1b196872a9
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-20 10:33:52 -0300

    Update 072.py

--- 072.py
+++ 072.py
@@ -2,3 +2,4 @@ cont = ('zero', 'um', 'dois', 'três', 'quatro',
         'cinco', 'seis', 'sete', 'oito', 'nove',
         'dez', 'onze', 'doze', 'treze', 'catorze',
         'quinze', 'dezesseis', 'dezessete', 'dezoito',
+        'dezenove', 'vinte')
