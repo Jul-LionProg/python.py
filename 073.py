b7d5ee94ec0d23eea0eb9d15b1f83902ed52794b
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-21 14:10:27 -0300

    Update 073.py

--- 073.py
+++ 073.py
@@ -18,3 +18,6 @@ print(f'O Chapecoense está na {times.index("Chapecoense")+1}ª posição')
 
 """
 EXERCÍCIO 073: Tuplas com Times de Futebol
+
+Crie uma tupla preenchida com os 20 primeiros colocados da Tabela
+do Campeonato 
