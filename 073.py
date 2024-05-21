d7c387d5bb1d36f5b1493dd28a4ba8754fe0c850
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-21 14:11:03 -0300

    Update 073.py

--- 073.py
+++ 073.py
@@ -22,3 +22,5 @@ EXERCÍCIO 073: Tuplas com Times de Futebol
 Crie uma tupla preenchida com os 20 primeiros colocados da Tabela
 do Campeonato Brasileiro de Futebol, na ordem de colocação.
 Depois mostre:
+
+A) Os 5 primeiros.
