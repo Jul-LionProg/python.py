dcf7ca6c37071f0dc68b3df07e4409eead6b5391
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-22 11:29:14 -0300

    Update 074.py

--- 074.py
+++ 074.py
@@ -4,3 +4,4 @@ numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
 print('Os valores sorteados foram: ', end='')
 for n in numeros:
     print(f'{n} ', end='')
+print(f'\nO maior valor sorteado foi {max(numeros)}')
