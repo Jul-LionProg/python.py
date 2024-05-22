099c56775d1e4548c9745f12fc50af2577c7f6e9
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-22 11:28:47 -0300

    Update 074.py

--- 074.py
+++ 074.py
@@ -2,3 +2,4 @@ from random import randint
 numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
            randint(1, 10), randint(1, 10))
 print('Os valores sorteados foram: ', end='')
+for n in numeros:
