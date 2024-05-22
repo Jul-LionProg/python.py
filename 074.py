511eb4b89a60516301aa6e30c3abf87532263a1a
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-22 11:30:08 -0300

    Update 074.py

--- 074.py
+++ 074.py
@@ -12,3 +12,4 @@ print(f'O menor valor sorteado foi {min(numeros)}')
 EXERCÍCIO 074: Maior e Menor Valores em Tupla
 
 Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
+Depois disso, mostre a listagem de números gerados e também indique o menor e o
