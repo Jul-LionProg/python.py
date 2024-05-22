08981c130c041ea5f50e49f9ee489e07aeafc853
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-22 11:30:18 -0300

    Update 074.py

--- 074.py
+++ 074.py
@@ -13,3 +13,5 @@ EXERCÍCIO 074: Maior e Menor Valores em Tupla
 
 Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 Depois disso, mostre a listagem de números gerados e também indique o menor e o
+maior valor que estão na tupla.
+"""
