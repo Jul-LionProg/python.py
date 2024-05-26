843393a93f2117fa157b1ff5d3fc87aa1277dd37
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-26 14:35:59 -0300

    Update 075.py

--- 075.py
+++ 075.py
@@ -5,3 +5,4 @@ num = (int(input('Digite um número: ')),
 print(f'Você digitou os valores {num}')
 print(f'O valor 9 apareceu {num.count(9)} vezes')
 if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição')
