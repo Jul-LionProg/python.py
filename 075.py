56d2e865176c7245d6ed3cedbf1a0834c533b6ca
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-26 14:37:21 -0300

    Update 075.py

--- 075.py
+++ 075.py
@@ -18,3 +18,4 @@ EXERCÍCIO 075: Análise de Dados em uma Tupla
 
 Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
 A) Quantas vezes apareceu o valor 9.
+B) Em que posição foi digitado o primeiro valor 3.
