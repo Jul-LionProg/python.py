d7b3519961d4ffd9e362fd67b538dcde0412fd07
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-26 14:35:40 -0300

    Update 075.py

--- 075.py
+++ 075.py
@@ -3,3 +3,4 @@ num = (int(input('Digite um número: ')),
        int(input('Digite mais um número: ')),
      int(input('Digite o último número: ')))
 print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
