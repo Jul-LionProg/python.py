5f62961f760b3357bca724f8e7eacdbb20ccd38a
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-26 14:36:14 -0300

    Update 075.py

--- 075.py
+++ 075.py
@@ -7,3 +7,4 @@ print(f'O valor 9 apareceu {num.count(9)} vezes')
 if 3 in num:
     print(f'O valor 3 apareceu na {num.index(3)+1}ª posição')
 else:
+    print('O valor 3 não foi digitado em nenhuma posição')
