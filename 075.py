318e32e9878b52b58e94c5b375256c8f330ada56
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-26 14:35:29 -0300

    Update 075.py

--- 075.py
+++ 075.py
@@ -2,3 +2,4 @@ num = (int(input('Digite um número: ')),
        int(input('Digite outro número: ')),
        int(input('Digite mais um número: ')),
      int(input('Digite o último número: ')))
+print(f'Você digitou os valores {num}')
