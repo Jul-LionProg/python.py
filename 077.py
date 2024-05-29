7412769f89759253d25d66dbe4c5c9cb71e05b69
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-05-29 06:41:23 -0300

    Update 077.py

--- 077.py
+++ 077.py
@@ -2,4 +2,5 @@ palavras = ('aprender', 'programar', 'linguagem', 'python',
             'curso', 'gratis', 'estudar', 'praticar',
             'trabalhar', 'mercado', 'programador', 'futuro')
 for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
 
