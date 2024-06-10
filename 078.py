96347428566ca332630700ab7927ba4bcfeba103
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-10 16:34:13 -0300

    Update 078.py

--- 078.py
+++ 078.py
@@ -21,3 +21,11 @@ for i, v in enumerate(listanum):
     if v == men:
         print(f'{i}... ', end='')
 print()
+
+
+"""
+EXERCÍCIO 078: Maior e Menor Valores na Lista
+
+Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre
+qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+"""
