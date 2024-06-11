b864207523b160f692502d2c2cd2dfab5a1304db
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-11 10:39:04 -0300

    Update 079.py

--- 079.py
+++ 079.py
@@ -1,11 +1,11 @@
 numeros = list()
 while True:
-  n = int(input('Digite um valor: '))
-  if n not in numeros:
-      numeors.append(n)
-      print('Valor adicionado com sucesso...')
-  else:
-      print('Valor duplicado! Não vou adicionar...')
-  r = str(input('Quer continuar[S/N]?'))
-  if r in 'Nn':
-      break
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeors.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar[S/N]?'))
+    if r in 'Nn':
+        break
