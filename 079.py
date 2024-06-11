835688c4bf0da0ccca0307059808e3cf7c15a720
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-11 10:35:16 -0300

    Update 079.py

--- 079.py
+++ 079.py
@@ -2,6 +2,7 @@ numeros = list()
 while True:
   n = int(input('Digite um valor: '))
   if n not in numeros:
-    numeors.append(n)
-    print('Valor duplicado! Não vou adicionar...')
-    
+      numeors.append(n)
+      print('Valor duplicado! Não vou adicionar...')
+  else:
+      print('Valor duplicado! Não vou adicionar...')
