d7bf7e6c59424448a5fbdf8ba62ae8a705b3192f
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-13 11:09:27 -0300

    Update 081.py

--- 081.py
+++ 081.py
@@ -1,15 +1,16 @@
 valores = []
 while True:
-  valores.append(int(input('digite um valor: ')))
-  resp = str(input('Quer continuar [Y/N]? '))
-  if resp in 'Nn':
-      break
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar [S/N]? '))
+    if resp in 'Nn':
+        break
 print('-=' * 30)
-print(f'Voce digite {len(valores)} elementos. ')
+print(f'Você digitou {len(valores)} elementos.')
 valores.sort(reverse=True)
-print(f'Os valor em ordem decrescente são {valores}.')
+print(f'Os valores em ordem decrescente são {valores}.')
 if 5 in valores:
-  print('O valor 5 faz parte da linha!')
+    print('O valor 5 faz parte da lista!')
 else:
-  print('O valor 5 não foi encontrado na lista!')
-  
+    print('O valor 5 não foi encontrado na lista!')
+
+
