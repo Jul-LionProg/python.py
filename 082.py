8efd3c36e1e9fbc9a2941292d4fd4cf4302a286a
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-22 11:10:40 -0300

    Update 082.py

--- 082.py
+++ 082.py
@@ -1,20 +1,23 @@
-num =  list()
+num = list()
 pares = list()
 impares = list()
-while = True:
-  num.append(int(imput('Digite um numero: ')))
-  resp = str(input('quer continuar [N/Y]'))
-  if resp in 'nN':
-    break
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar [S/N]? '))
+    if resp in 'Nn':
+        break
 for i, v in enumerate(num):
-  if v % 2 == 0:
-    pares.append(v)
-  elif v % 2 == 1:
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
         impares.append(v)
-print('-= *' 30)
+print('-=' * 30)
 print(f'A lista completa é {num}')
 print(f'A lista de pares é {pares}')
-print(f'{A lista de impares é {impare)
+print(f'A lista de ímpares é {impares}')
+
+
+
 '''
 DESAFIO 082
 Crie um programa que vai ler varios numeros e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores impares
