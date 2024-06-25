9ab7c5a82d1007cd35594a337d98bb3ecf3ade3b
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-25 09:41:06 -0300

    Update 085.py

--- 085.py
+++ 085.py
@@ -11,3 +11,10 @@ num[0].sort()
 num[1].sort()
 print(f'Os valores pares digitados foram: {num[0]}')
 print(f'Os valores ímpar
+
+
+'''
+DASAFIO 05
+Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha
+separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente. 
+'''
