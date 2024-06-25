92411448c0ecff1e4969f9a90ae9f19ffb962cad
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-25 09:28:41 -0300

    Update 085.py

--- 085.py
+++ 085.py
@@ -3,4 +3,6 @@ valor = 0
 for c in range(1, 8):
   valor = int(input(f'digite o {c}º valor: '))
   if valor % 2 == 0:
-        nm[0].append(valor)
+      num[0].append(valor)
+else:
+  num[0].append(valor)
