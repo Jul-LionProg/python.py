32bdebb6fc283fd7b2c9cc7700fe4c19c87e62c6
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-06-26 08:38:50 -0300

    Update 086.py

--- 086.py
+++ 086.py
@@ -1,4 +1,6 @@
 matriz = [[0, 0, 0],[0, 0,0],[0, 0, 0]]
 for l in range(0, 3):
     for c  in range(0, 3):
-        matriz[l][c] = int(input(input(f'Digite um valor para [{l},{c}]: ')))
+        matriz[l][c] = int(inputt(f'Digite um valor para [{l},{c}]: '))
+print('-=' * 30)
+
