35338316ca88fadc163115501dc5f9213ea48f2c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-07 10:13:53 -0300

    Update 088.py

--- 088.py
+++ 088.py
@@ -20,8 +20,8 @@ while tot <= quant:
     jogos.append(lista[:])
     lista.clear()
     tot += 1
-print('-=' * 3, f'SORTEANDO {quant} ','-=' * 3)
-
+print('-=' * 3, f' SORTEANDO {quant} JOGOS ', '-=' * 3)
+for i, l in enumerate(jogos):
 
 '''
 DESAFIO 088: Palpites Para a Mega Sena
