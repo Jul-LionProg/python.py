9148b4f16c8470656f3d6ac2b63118015f52662c
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-07 10:18:04 -0300

    Update 088.py

--- 088.py
+++ 088.py
@@ -24,6 +24,8 @@ print('-=' * 3, f' SORTEANDO {quant} JOGOS ', '-=' * 3)
 for i, l in enumerate(jogos):
     print(f'Jogo {i + 1}: {l}')
     sleep(1)
+print('-=' * 5, '< BOA SORTE! > ', '-=' * 5)
+
 
 '''
 DESAFIO 088: Palpites Para a Mega Sena
