a04d52c03c72e5760a3da9869a5c5e693a146971
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-07 10:07:34 -0300

    Update 088.py

--- 088.py
+++ 088.py
@@ -3,37 +3,28 @@ from time import sleep
 lista = list()
 jogos = list()
 print('-' * 30)
-print('   JOGA NA MGA SENA   ')
+print('      JOGA NA MEGA SENA      ')
 print('-' * 30)
-quant = int(input('Quantos jogos voce quer que eu sorteie?'))
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
 tot = 1
 while tot <= quant:
-  cont = 0
-  while True:
-    num = randint(1, 60)
-    if num not in liste:
-      liste.append(num)
-      cont += 1
-    if cont >= 6:
-      break
-lista.sort()
-jogos.append(lista[:])
-lista.clead()
-tot += 1
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+
+
 
 '''
-DESAFIO 088
-EXERCÍCIO 087: Mais Sobre Matriz em Python
-
-Aprimore o desafio anterior, mostrando no final:
-
-A) A soma de todos os valores pares digitados.
-B) A soma dos valores da terceira coluna.
-C) O maior valor da segunda linha.
-
-0 [_][_][_]
-1 [_][_][_]
-2 [_][_][_]
-   0  1  2
-
+DESAFIO 088: Palpites Para a Mega Sena
+Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos
+serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 '''
