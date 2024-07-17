c5b154fd4f0f9a71ad1d17adbf9ca9df868af284
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-17 09:57:25 -0300

    Update 090.py

--- 090.py
+++ 090.py
@@ -4,3 +4,4 @@ aluno['media'] = str(input('Nome: '))
 aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
 if aluno['media'] >= 7:
     aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
