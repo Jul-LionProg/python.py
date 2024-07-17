24f946790147dd3ed75c95e3e3d7ab61b27a03c0
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-17 10:00:11 -0300

    Update 090.py

--- 090.py
+++ 090.py
@@ -5,3 +5,4 @@ aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
 if aluno['media'] >= 7:
     aluno['situação'] = 'Aprovado'
 elif 5 <= aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
