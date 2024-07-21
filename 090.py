1893e10733ea826a4c821b2f04f7ceb5c7c833ce
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-21 15:28:59 -0300

    Update 090.py

--- 090.py
+++ 090.py
@@ -1,13 +1,13 @@
-aluno = disct()
-aluno['nome']
-aluno['media'] = str(input('Nome: '))
-aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
-if aluno['media'] >= 7:
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
     aluno['situação'] = 'Aprovado'
 elif 5 <= aluno['média'] < 7:
     aluno['situação'] = 'Recuperação'
 else:
     aluno['situação'] = 'Reprovado'
 print('-=' * 30)
-for k, v in aluno.ittem():
-    print(f'  -{k} é igual a {v}')
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
+
