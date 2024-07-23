d3fc2422009da04825a6bc1f63fe73f7be870c3d
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-23 16:14:58 -0300

    Update 091.py

--- 091.py
+++ 091.py
@@ -17,4 +17,8 @@ for i, v in enumerate(ranking):
     print(f'   {i + 1}º lugar: {v[0]} com {v[1]}.')
     sleep(1)
 
+'''
 DESAFIO 091:
+Crie um programa nde 4 jogadores joguem um dado e tenham resultado aleatorios. Guarde esses resultdos em um
+dicionario. No final, coloque esse dicionario em ordem, sabendo que o vencedor tirou o maior numero no dado.
+'''
