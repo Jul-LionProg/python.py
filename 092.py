29391ed51d2fcd855d5e7206ded37259effc3b73
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-24 20:56:01 -0300

    Update 092.py

--- 092.py
+++ 092.py
@@ -3,4 +3,5 @@ dados = dict()
 dodas['nome'] = str(input('Nome: '))
 nasc = int(input('Ano de nascimento: '))
 dados['idade'] = datetime.now().year - nasc
-dados['ctps'] != 0:
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['cpts'] != 0:
