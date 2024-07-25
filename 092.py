d2e5fc932091b6929cb286f24b3dd0bcf6f55ce4
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-24 21:15:32 -0300

    Update 092.py

--- 092.py
+++ 092.py
@@ -1,22 +1,22 @@
 from datetime import datetime
 dados = dict()
-dodas['nome'] = str(input('Nome: '))
+dados['nome'] = str(input('Nome: '))
 nasc = int(input('Ano de nascimento: '))
 dados['idade'] = datetime.now().year - nasc
 dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
-if dados['cpts'] != 0:
-  dados['contratação'] = int(input('Ano de contratação: '))
-  dados['salario'] = float(input('Salario: R$ '))
-  dados['aponsentadoria'] = dados['idade'] + ((dados[contratação] + 35) - datetime.now().year)
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$ '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
 print('-=' * 30)
 for k, v in dados.items():
-    print(f'  - {k} tem o valor{v}')
+    print(f'  - {k} tem o valor {v}')
 
 
+"""
+DESAFIO 092: Cadastro de Trabalhador em Python
 
-'''
-DESAFIO 092:
-Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionario.
-Se por acasso a CPTSfor diferente de zero, o dicionario recebera tambem o ano de contratação e o salario.
-calcule e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar.
-'''
+Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário.
+Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
+Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+"""
