04382bc2e5af8a11cf843a9e9b5bb962b3870a3b
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-24 21:03:35 -0300

    Update 092.py

--- 092.py
+++ 092.py
@@ -7,3 +7,4 @@ dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
 if dados['cpts'] != 0:
   dados['contratação'] = int(input('Ano de contratação: '))
   dados['salario'] = float(input('Salario: R$ '))
+  dados['aponsentadoria'] = dados['idade'] + ((dados[contratação] + 35) - datetime.now().year)
