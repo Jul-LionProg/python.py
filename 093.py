7a35f8db5d4d1f96de6fb62e4ea931690a8cfc37
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-25 10:37:54 -0300

    Update 093.py

--- 093.py
+++ 093.py
@@ -3,4 +3,4 @@ partidas = list()
 jogador['nome'] = str(input('Nome do Jogaodr: '))
 tot = int(input(f'Quantas partidas {jogoador["nome"]} jogou? '))
 for c in range(0, tot):
-    partidas.append(int(input(f'   quantos gols na partidas')))
+    partidas.append(int(input(f'   quantos gols na partidas {c}? ')))
