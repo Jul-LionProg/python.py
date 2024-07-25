7fed82bd67b9e6901692f416014fb3863a9a4207
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-25 11:04:54 -0300

    Update 093.py

--- 093.py
+++ 093.py
@@ -14,4 +14,11 @@ for k, v in jogador.items():
 print('-=' * 30)
 print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
 for i, v in enumerate(jogador['gols']):
-    print(f'     => Na partida {i}, fez {v} gols')
+    print(f'     => Na partida {i}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
+
+'''
+DESAFIO 093:
+Crei um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. 
+Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso sera guardado em um dicionario, incluindo o total de gols feitos durante o campionato.
+'''
