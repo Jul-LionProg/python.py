8b6d0976bd5b43dece4a4a87ad2d0bfdfb54ed75
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-07-29 16:39:31 -0300

    Update 094.py

--- 094.py
+++ 094.py
@@ -2,12 +2,12 @@ galera = list()
 pessoa = dict()
 soma = media = 0
 while True:
-  pessoa.clear()
-  pessoa['nome'] = str(input('Nome: '))
-  while True:
-    pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper()[0]
-    if pessoa['sexo'] in 'MF':
-      break
-    print('ERRO! Por favor, digite apenas M ou F.')
-pessoa['idade'] = int(input('Idade: '))
-soma += pessoa['idade']
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
